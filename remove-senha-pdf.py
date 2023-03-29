197e631b309c74b50cf11f09e650cfd554256091
Renato Monteiro Batista <30217311+renatomb@users.noreply.github.com>
2023-03-29 14:18:41 -0300

    Senha informada como parâmetro de linha de execução. Correção dos erros PyPDF2.errors.DeprecationError. Processar .PDF além dos .pdf.

--- remove-senha-pdf.py
+++ remove-senha-pdf.py
@@ -1,18 +1,27 @@
 import os
-from PyPDF2 import PdfFileReader, PdfFileWriter
+import sys
+from PyPDF2 import PdfReader, PdfWriter
+
+# verifica se a senha foi fornecida como argumento de linha de comando
+if len(sys.argv) < 2:
+    print('Por favor, forneça uma senha como argumento. Ex.: python ' + sys.argv[0] + ' minha_senha')
+    sys.exit()
+
+# obtém a senha do primeiro argumento da linha de comando
+password = sys.argv[1]
 
 # define a função para remover a senha
 def remove_password(filename):
     with open(filename, 'rb') as f:
-        pdf = PdfFileReader(f)
+        pdf = PdfReader(f)
         # verifica se o arquivo possui senha
-        if pdf.isEncrypted:
+        if pdf.is_encrypted:
             # tenta remover a senha com uma senha em branco
-            pdf.decrypt('')
+            pdf.decrypt(password)
             # escreve o PDF sem senha em um novo arquivo
-            output = PdfFileWriter()
-            for i in range(pdf.getNumPages()):
-                output.addPage(pdf.getPage(i))
+            output = PdfWriter()
+            for page in pdf.pages:
+                output.add_page(page)
             with open('unencrypted_' + filename, 'wb') as out:
                 output.write(out)
             print('Senha removida de ', filename)
@@ -20,7 +29,7 @@ def remove_password(filename):
             print('O arquivo', filename, 'não possui senha.')
 
 # obtem a lista de arquivos PDF na pasta atual
-pdf_files = [f for f in os.listdir('.') if f.endswith('.pdf')]
+pdf_files = [f for f in os.listdir('.') if f.lower().endswith('.pdf')]
 
 # remove a senha de cada arquivo PDF
 for filename in pdf_files:
