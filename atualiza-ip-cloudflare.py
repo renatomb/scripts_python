dc0f63a804db4ed6ecafc5415ccd52ec9777bcb4
Renato Monteiro Batista <30217311+renatomb@users.noreply.github.com>
2023-06-27 09:19:32 -0300

    Separa arquivo de configuracao

--- atualiza-ip-cloudflare.py
+++ atualiza-ip-cloudflare.py
@@ -1,38 +1,72 @@
 import requests
 import json
+import configparser
+from pathlib import Path
 
-# Dados secretos do usuario
-## O identificador da zona pode ser obtido na pagina de overview do dominio
-## O record_id pode ser obtido no endpoint: https://api.cloudflare.com/client/v4/zones/{{zone_id}}/dns_records
-## x_auth_key pode ser obtido na pagina https://dash.cloudflare.com/profile/api-tokens
-ZONE_ID='identificador_da_zona' 
-RECORD_ID='identificador_do_registro_dns'
-X_AUTH_EMAIL='email@example.com'
-X_AUTH_KEY='api_key_global_da_conta'
+path = Path('cfg_atualiza-ip.ini')
+if path.is_file():
+    # lê o arquivo de configuração
+    config = configparser.ConfigParser()
+    config.read(path)
+    # verifica se existe configuracao de ipv4
+    if config.has_section('ipv4'):
+        ZONE_ID = config['ipv4']['zone_id']
+        RECORD_ID = config['ipv4']['record_id']
+        HOST_RECORD = config['ipv4']['host_record']
+        X_AUTH_EMAIL = config['ipv4']['x_auth_email']
+        X_AUTH_KEY = config['ipv4']['x_auth_key']
+        print("Buscando ipv4...")
+        # Obtém o IPv4 atual da internet através do site ipinfo.io/json
+        response = requests.get('https://ipinfo.io/json')
+        ip = json.loads(response.text)['ip']
+        # Realiza uma requisição POST para a API do Cloudflare, atualizando o registro DNS de "sub.nome.com" com o IP obtido
+        url = "https://api.cloudflare.com/client/v4/zones/{ZONE_ID}/dns_records/{RECORD_ID}".format(ZONE_ID=ZONE_ID, RECORD_ID=RECORD_ID)
+        headers = {
+            'Content-Type': 'application/json',
+            'X-Auth-Email': '{X_AUTH_EMAIL}'.format(X_AUTH_EMAIL=X_AUTH_EMAIL),
+            'X-Auth-Key': '{X_AUTH_KEY}'.format(X_AUTH_KEY=X_AUTH_KEY)
+        }
+        data = {
+            'type': 'A',
+            'name': '{HOST_RECORD}'.format(HOST_RECORD=HOST_RECORD),
+            'content': ip,
+            'ttl': 1
+        }
+        response = requests.put(url, headers=headers, json=data)
+        print(response.text)
+    else:
+        # trata o caso de seção não encontrada
+        print("Nenhum ipv4 a atualizar")
+    # verifica se existe configuracao de ipv6
+    if config.has_section('ipv6'):
+        ZONE_ID = config['ipv6']['zone_id']
+        RECORD_ID = config['ipv6']['record_id']
+        HOST_RECORD = config['ipv6']['host_record']
+        X_AUTH_EMAIL = config['ipv6']['x_auth_email']
+        X_AUTH_KEY = config['ipv6']['x_auth_key']
+        print("Buscando ipv6...")
+        # Obtém o IPv6 atual da internet através do site https://ipv6.seeip.org/jsonip
+        response = requests.get('https://ipv6.seeip.org/jsonip')
+        ip = json.loads(response.text)['ip']
+                # Realiza uma requisição POST para a API do Cloudflare, atualizando o registro DNS de "sub.nome.com" com o IP obtido
+        url = "https://api.cloudflare.com/client/v4/zones/{ZONE_ID}/dns_records/{RECORD_ID}".format(ZONE_ID=ZONE_ID, RECORD_ID=RECORD_ID)
+        headers = {
+            'Content-Type': 'application/json',
+            'X-Auth-Email': '{X_AUTH_EMAIL}'.format(X_AUTH_EMAIL=X_AUTH_EMAIL),
+            'X-Auth-Key': '{X_AUTH_KEY}'.format(X_AUTH_KEY=X_AUTH_KEY)
+        }
+        data = {
+            'type': 'AAAA',
+            'name': '{HOST_RECORD}'.format(HOST_RECORD=HOST_RECORD),
+            'content': ip,
+            'ttl': 1
+        }
+        response = requests.put(url, headers=headers, json=data)
+        print(response.text)
+    else:
+        # trata o caso de seção não encontrada
+        print("Nenhum ipv6 a atualizar")
+else:
+    # trata o caso de arquivo não encontrado
+    print("cfg_atualiza-ip.ini inexistente")
 
-# Obtém o IPv4 atual da internet através do site ipinfo.io/json
-response = requests.get('https://ipinfo.io/json')
-
-## Para obter o IPv6 atual pode-se usar o site 
-## response = requests.get('https://ipv6.seeip.org/jsonip')
-ip = json.loads(response.text)['ip']
-
-# Realiza uma requisição POST para a API do Cloudflare, atualizando o registro DNS de "sub.nome.com" com o IP obtido
-url = "https://api.cloudflare.com/client/v4/zones/{ZONE_ID}/dns_records/{RECORD_ID}".format(ZONE_ID=ZONE_ID, RECORD_ID=RECORD_ID)
-headers = {
-    'Content-Type': 'application/json',
-    'X-Auth-Email': '{X_AUTH_EMAIL}'.format(X_AUTH_EMAIL=X_AUTH_EMAIL),
-    'X-Auth-Key': '{X_AUTH_KEY}'.format(X_AUTH_KEY=X_AUTH_KEY)
-}
-data = {
-## Para ipV6 o tipo de registro deve ser AAAA
-# 'type': 'AAAA',
-    'type': 'A',
-    'name': 'host.example.com',
-    'content': ip,
-    'ttl': 1
-}
-response = requests.put(url, headers=headers, json=data)
-
-# Exibe o resultado da requisição
-print(response.text)
